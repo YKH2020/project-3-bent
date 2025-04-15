3c7e6f229872f689c1c145da5508b89a00db9c36
Aryan Laxman Sirohi <aryanlaxmansingh22@gmail.com>
2025-04-14 20:59:15 -0400

    updated deep learning implementations and comments

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
-from time import sleep
 from naive import recommend_top_joke
 from get_jokes import read_clean_joke_text
 from input_joke import get_best_joke_for_user
+from matrix_factorization_inference import inference
 
+# mapping user names to ids
 users = {
     "Bill": 2,
     "Tejas": 3,
@@ -12,19 +13,37 @@ users = {
 }
 
 def run_naive_approach(user):
+    '''
+    This function calls the naive approach predict method
+
+    Args
+        user: name
+    '''
     with st.spinner(text="Processing", show_time=True):
         joke_id = recommend_top_joke("user_" + str(users.get(user)))
         st.write(read_clean_joke_text(joke_id)[joke_id])
 
 def run_non_dl(user):
+    '''
+    This function calls the PMF predict method
+
+    Args
+        user: name
+    '''
     with st.spinner(text="Processing", show_time=True):
-        joke_id = get_best_joke_for_user("user_" + str(users.get(user)))
+        joke_id = inference(users.get(user))
         st.write(read_clean_joke_text(joke_id)[joke_id])
 
 def run_dl():
-    with st.spinner(text="Processing", show_time=True):
-        sleep(5)
+    '''
+    This function calls the deep learning predict method
 
+    Args
+        user: name
+    '''
+    with st.spinner(text="Processing", show_time=True):
+        joke_id = get_best_joke_for_user("user_" + str(users.get(user)))
+        st.write(read_clean_joke_text(joke_id)[joke_id])
 
 st.title("Joke Recommender")
 st.write("This tool recommends jokes for users based on their ratings on a set of 10 jokes")
@@ -40,8 +59,8 @@ user = st.selectbox(
 st.header("The Naive Approach")
 st.write("For the naive approach we built a mean model in order to recommend the top joke for " + f'{user}')
 
-# if st.button("Run Naive Approach"):
-    # run_naive_approach(user)
+if st.button("Run Naive Approach"):
+    run_naive_approach(user)
 
 st.header("Traditional ML Method")
 st.write("In the non-deep learning method we used probabilistic matrix factorization to recommend the top joke for " + f'{user}')
@@ -53,4 +72,4 @@ st.header("Deep Learning Method")
 st.write("This model was a custom autoencoder used to train on our dataset to recommend the top joke for " + f'{user}')
 
 if st.button("Run Autoencoder"):
-    run_dl()
+    run_dl(user) 
